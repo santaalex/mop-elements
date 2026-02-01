0b508ec66127a4a8a58f34f97edbb88256a82a72
santaalex <40752008@qq.com>
2026-02-01 17:58:28 +0800

    Add files via upload

--- packages/mop-drag-sdk/NodeDragStrategy.js
+++ packages/mop-drag-sdk/NodeDragStrategy.js
@@ -1,4 +1,4 @@
-import { BaseStrategy } from 'https://cdn.jsdelivr.net/gh/santaalex/mop-elements@main/interactions/strategies/BaseStrategy.js';
+import { BaseStrategy } from '../../../interactions/strategies/BaseStrategy.js';
 // SDK Internal Import
 import { DragCore } from './DragCore.js';
 
